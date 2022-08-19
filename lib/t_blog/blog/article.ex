14df499b51d5647051744114b6eb658c60844650
Helder Barboza <helder.tiago@hotmail.com>
2022-08-19 16:38:03 -0400

    feat: add schema `articles`

---
+++ lib/t_blog/blog/article.ex
@@ -0,0 +1,20 @@
+defmodule TBlog.Blog.Article do
+  use Ecto.Schema
+  import Ecto.Changeset
+
+  schema "articles" do
+    field :author, :string
+    field :content, :string
+    field :tags, {:array, :string}
+    field :title, :string
+
+    timestamps()
+  end
+
+  @doc false
+  def changeset(article, attrs) do
+    article
+    |> cast(attrs, [:title, :author, :content, :tags])
+    |> validate_required([:title, :content, :tags])
+  end
+end
--- lib/t_blog_web/router.ex
+++ lib/t_blog_web/router.ex
@@ -18,6 +18,7 @@ defmodule TBlogWeb.Router do
     pipe_through :browser
 
     get "/", PageController, :index
+    resources "/articles", ArticleController
   end
 
   # Other scopes may use custom stacks.
